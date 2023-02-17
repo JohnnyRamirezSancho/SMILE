df53afe6e63f17577b3e0e8e297ef30792792bda
David Daza <drdaza18@gmail.com>
2023-02-17 12:55:04 +0100

    added payload

---
+++ src/main/java/com/smile/smile/payloads/PatientPayload.java
@@ -0,0 +1,97 @@
+package com.smile.smile.payloads;
+
+import java.util.Date;
+
+public class PatientPayload {
+
+    private String dni;
+    private String name;
+    private String lastname;
+    private Date birthdate;
+
+    private String profile;
+    private String phone;
+    private String address;
+    private String city;
+
+    public PatientPayload() {
+    }
+
+    public PatientPayload(String dni, String name, String lastname, Date birthdate, String profile, String phone,
+            String address, String city) {
+        this.dni = dni;
+        this.name = name;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.profile = profile;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    
+}
